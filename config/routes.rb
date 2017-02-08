65fd7b3c7d9c976ef44a36918078daa42bc3be74
SiamKing <thesiameseelectric@gmail.com>
2017-02-08 14:30:20 -0800

    created routes for events

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,11 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :api do
+    namespace :v1 do
+      resources :events
+    end
+  end
+
   root to: 'application#angular_home'
 end
