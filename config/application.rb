9bb9438e0cf8c4d96962875d2bdeeaa8cf209190
SiamKing <thesiameseelectric@gmail.com>
2017-02-15 09:35:12 -0800

    made changes to push to heroku

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+
 module Meditations
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -13,3 +14,19 @@ module Meditations
     # -- all .rb files in that directory are automatically loaded.
   end
 end
+
+# via https://gist.github.com/afeld/5704079
+# Rails.application.configure do
+#   config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+#   # We don't want the default of everything that isn't js or css, because it pulls too many things in
+#   config.assets.precompile.shift
+#
+#   # Explicitly register the extensions we are interested in compiling
+#   config.assets.precompile.push(Proc.new do |path|
+#     File.extname(path).in? [
+#       '.html', '.erb', '.haml',                 # Templates
+#       '.png',  '.gif', '.jpg', '.jpeg', '.svg', # Images
+#       '.eot',  '.otf', '.svc', '.woff', '.ttf', # Fonts
+#     ]
+#   end)
+# end
