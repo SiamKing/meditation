e6b1b9ab6792c019fc63242ed70455225d4ec116
SiamKing <thesiameseelectric@gmail.com>
2017-02-18 15:45:06 -0800

    changed event serializer for meditaiton association to include just name and id

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -2,4 +2,7 @@ class EventSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :meditation_id, :minutes, :date
   has_one :meditation, only: :name
   has_one :user
+  class MeditationSerializer < ActiveModel::Serializer
+    attributes :id, :name
+  end
 end
