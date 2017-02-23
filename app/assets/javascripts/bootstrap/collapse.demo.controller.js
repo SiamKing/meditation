9e47e9a08036f35622d5e54002fcb8146200ca2a
SiamKing <thesiameseelectric@gmail.com>
2017-02-23 14:35:29 -0800

    deleted unneeded code from navbar ctrl

--- app/assets/javascripts/bootstrap/collapse.demo.controller.js
+++ app/assets/javascripts/bootstrap/collapse.demo.controller.js
@@ -7,13 +7,6 @@
       $scope.isNavCollapsed = true;
       $scope.isCollapsed = false;
       $scope.isCollapsedHorizontal = false;
-      $rootScope.hideDiv = hideDiv;
-      $rootScope.hideLink = true;
 
-      function hideDiv() {
-        console.log("Hi")
-        return $rootScope.hideLink = true;
-    }
-    $rootScope.link = false
   });
 }())
