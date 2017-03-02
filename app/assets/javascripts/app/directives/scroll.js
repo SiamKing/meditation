4b287cbda8d5d9276189a4205df34fd575580ef6
SiamKing <thesiameseelectric@gmail.com>
2017-03-02 14:58:52 -0800

    created directive for scrolling

---
+++ app/assets/javascripts/app/directives/scroll.js
@@ -0,0 +1,17 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('meditation')
+    .directive('ScrollDirective', [function() {
+      return {
+        template: [
+        `<div class="btn-center">
+          <button type="button" class="btn btn-default" ng-click="gotoElement('top')">Go to top</button>
+        </div>`
+      ],
+      controller: 'ScrollController',
+      controllerAs: 'vm'
+    }
+    }])
+}())
