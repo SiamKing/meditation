9970b6a9d039b637b156ccfe7dc36a8dc447cadc
SiamKing <thesiameseelectric@gmail.com>
2017-03-15 10:31:28 -0700

    deleted nested route for events under users and disabled placeholder option for meditation filter

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :users do
-        resources :events
-      end
+      resources :users
     end
   end
 
