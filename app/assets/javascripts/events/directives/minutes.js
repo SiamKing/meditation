60ff6d6b09a26061a481028eb9174396223d5f4a
SiamKing <thesiameseelectric@gmail.com>
2017-03-15 20:31:59 -0700

    updated ng-message to show more clearly on page

--- app/assets/javascripts/events/directives/minutes.js
+++ app/assets/javascripts/events/directives/minutes.js
@@ -9,10 +9,22 @@
         bindToController: true,
         template: [
           `<div class="col-md-3 form-group" ng-class="{ 'has-error': form.minutes.$touched && form.minutes.$invalid}">
-            <input ng-if="!event.event.minutes" type="number" class="form-control minutes" name="minutes" placeholder="Minutes Meditating" ng-model="event.minutes" required="required">
-            <input ng-if="event.event.minutes" type="number" class="form-control minutes" name="minutes" placeholder="{{event.event.minutes}} minutes" ng-model="event.minutes" required="required">
-            <div ng-if="form.minutes.$invalid && form.minutes.$dirty" ng-messages="form.minutes.$error">
-              <div ng-message="required">Minutes are required</div>
+            <input ng-if="!event.event.minutes"
+                   type="number" min="1"
+                   class="form-control minutes"
+                   name="minutes"
+                   placeholder="Minutes Meditating"
+                   ng-model="event.minutes"
+                   required="required">
+            <input ng-if="event.event.minutes"
+                   type="number" min="1"
+                   class="form-control minutes"
+                   name="minutes"
+                   placeholder="{{event.event.minutes}} minutes"
+                   ng-model="event.minutes"
+                   required="required">
+            <div class="message-alert" ng-show="form.minutes.$touched && form.minutes.$invalid" ng-messages="form.minutes.$error" role="alert">
+              <div ng-message="required" style="align: 'center'">Please enter minutes</div>
             </div>
           </div>`
         ],
