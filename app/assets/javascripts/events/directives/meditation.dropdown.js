e28aa6ea1999b4a64397b10132da255a5fff37f2
SiamKing <thesiameseelectric@gmail.com>
2017-03-22 14:32:58 -0700

    created template for meditations dropdown html and moved html from directive there

--- app/assets/javascripts/events/directives/meditation.dropdown.js
+++ app/assets/javascripts/events/directives/meditation.dropdown.js
@@ -5,26 +5,10 @@
     .module('meditation')
     .directive('meditationDropdown', function() {
       return {
-        // scope: {
-        //   selectedMeditation: '@selectedMeditation'
-        // },
         controller: 'MeditationsDropdownController',
         controllerAs: 'vm',
         bindToController: true,
-        template: [
-              `<div class="col-md-4 meditations-dropdown-menu">
-                <span class="btn-group" uib-dropdown keyboard-nav>
-                  <button id="simple-btn-keyboard-nav" type="button" class="btn btn-meditations-dropdown-menu" uib-dropdown-toggle ng-disabled="disabled">
-                    {{selectedMeditation}} <span class="caret"></span>
-                  </button>
-                  <ul class="dropdown-menu" uib-dropdown-menu role="menu" aria-labelledby="simple-btn-keyboard-nav">
-                    <li ng-repeat="meditation in vm.meditations">
-                      <a href="" class="meditations-dropdown-menu" ng-click="meditationSelected(meditation.name) ; meditationId(meditation.id)">{{meditation.name}}</a>
-                    </li>
-                  </ul>
-                </span>
-              </div>`
-            ]
+        templateUrl: 'events/templates/meditations.dropdown.html'
       }
     })
 }())
